8b02e838a274c3c565a38f11256ce8ebbb1a92c0
yeferson castiblanco <y.castiblanco@inlaze.com>
2024-04-22 16:17:38 -0500

    feat(descrypt users)

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, func, LargeBinary
 from sqlalchemy.orm import sessionmaker
-from app.models.users import Base
-
+from app.models.users import Base, User
+from sqlalchemy.dialects.postgresql import BYTEA
 app = FastAPI()
 engine = create_engine("postgresql://admin:admin1234@localhost:5436/integrations?client_encoding=utf8", echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
@@ -10,4 +10,10 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 session = SessionLocal()
 
 Base.metadata.create_all(bind=engine)
-
+new_user = User('yeferson@example.com', 'Admin1234', 'yeferson', 'castiblanco', 1023456890)
+session.add(new_user)
+session.commit()
+key = 'Admin1234'
+document = 1023456890
+decrypted_email = session.query(func.pgp_sym_decrypt(User.email.cast(BYTEA), 'Admin1234')).all()
+print(decrypted_email, 'XD')
--- app/models/users.py
+++ app/models/users.py
@@ -1,5 +1,5 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, func
 
 Base = declarative_base()
 
@@ -7,15 +7,16 @@ Base = declarative_base()
 class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True)
-    email = Column(String(50), unique=True, index=True)
+    email = Column(String(500), unique=True, index=True)
     password = Column(String(500))
-    name = Column(String(15))
-    lastname = Column(String(15))
+    name = Column(String(500))
+    lastname = Column(String(500))
     document = Column(Integer)
-
+    #
     def __init__(self, email: str, password: str, name: str, lastname: str, document: float):
-        self.email = email
-        self.password = password
+        key = 'Admin1234'
+        self.email = func.pgp_sym_encrypt(email, key)
+        self.password = func.pgp_sym_encrypt(password, key)
         self.name = name
         self.lastname = lastname
         self.document = document
